9a53d793f23de97386e161743106a6fd7a402d55
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-02-28 17:49:16 -0500

    open flags for both all and creat with pie charts

--- src/open-flag-analyzer.py
+++ src/open-flag-analyzer.py
@@ -5,9 +5,10 @@ import json
 from constants import *
 import matplotlib.pyplot as plt
 import numpy as np
+import sys 
 
 # Distribution of the specific open flag (e.g., O_CREAT 0o100)
-flag_bit = 0o100
+creat_flag = 0o100
 
 # Plot
 sort_legend = True
@@ -32,36 +33,51 @@ with open('open_flag_filtered.json', 'w') as fout:
 """
 
 all_cnt_res = {}
+creat_cnt_res = {}
 
 # Distribution of ALL open flags
 for flag, cnt in open_flag_filtered.items():
     flag_num = bin(flag).count("1")
+    # #define O_RDONLY	00000000
+    if flag & 1 == 0:
+        flag_num += 1
     if flag_num in all_cnt_res.keys():
         all_cnt_res[flag_num] += cnt
     else:
-        all_cnt_res[flag_num] = cnt 
+        all_cnt_res[flag_num] = cnt     
+    # if O_CREAT is enabled
+    if flag & creat_flag == creat_flag:
+        if flag_num in creat_cnt_res.keys():
+            creat_cnt_res[flag_num] += cnt
+        else:
+            creat_cnt_res[flag_num] = cnt
 
-y_all_list = []
-all_labels = []
-for flag_num, cnt in all_cnt_res.items():
-    y_all_list.append(cnt)
-    all_labels.append(str(flag_num))
+# Plot all open flags and open flags that contain O_CREAT
+res_list = [all_cnt_res, creat_cnt_res]
+title_list = ['all', 'O_CREAT']
+for i in range(len(res_list)):
+    each_res = res_list[i]
+    y_all_list = []
+    all_labels = []
+    for flag_num, cnt in each_res.items():
+        y_all_list.append(cnt)
+        all_labels.append(str(flag_num))
 
-y_all = np.asarray(y_all_list)
-all_percents = 100.*y_all/y_all.sum()
+    y_all = np.asarray(y_all_list)
+    all_percents = 100.*y_all/y_all.sum()
 
-all_labels = ['{0} flags ({1:1.2f} %)'.format(i,j) for i,j in zip(all_labels, all_percents)]
+    all_labels = ['{0} flags ({1:1.2f} %)'.format(i,j) for i,j in zip(all_labels, all_percents)]
 
-patches, texts = plt.pie(y_all, startangle=90)
+    patches, texts = plt.pie(y_all, startangle=90)
 
-if sort_legend:
-    patches, all_labels, dummy =  zip(*sorted(zip(patches, all_labels, y_all),
-                                          key=lambda x: x[2],
-                                          reverse=True))
+    if sort_legend:
+        patches, all_labels, dummy =  zip(*sorted(zip(patches, all_labels, y_all),
+                                            key=lambda x: x[2],
+                                            reverse=True))
 
-plt.legend(patches, all_labels, loc='best', bbox_to_anchor=(-0.1, 1.),
-           fontsize=8)
+    plt.legend(patches, all_labels, loc='best', bbox_to_anchor=(-0.1, 1.),
+            fontsize=8)
 
-plt.title('Number of flags (bits) for open [xfstests ext4 all, filtered]')
-plt.savefig('./Assets/Analysis-Figures/filtered-open-flags.pdf', bbox_inches='tight',dpi=plot_dpi)
-plt.close('all')
+    plt.title('Number of flags (bits) for open {} [xfstests ext4 all, filtered]'.format(title_list[i]))
+    plt.savefig('./Assets/Analysis-Figures/filtered-open-flags-{}.pdf'.format(title_list[i]), bbox_inches='tight',dpi=plot_dpi)
+    plt.close('all')
