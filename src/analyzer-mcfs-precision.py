2dd2dbd2aa0efaa0c17f86fff4a91cb0b05e02ac
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-03-11 17:20:02 -0500

    Change accuracy to precision

--- src/analyzer-mcfs-precision.py
+++ src/analyzer-mcfs-precision.py
@@ -4,9 +4,9 @@ from constants import *
 from analyzerutils import *
 
 # MCFS may contain many sequence files, so we need to analyze them all
-mcfs_seqs = ['sequence-pan-20230304-195136-724583.log']
+mcfs_seqs = ['sequence-pan-20230311-005751-2523268.log']
 
-mcfs_pkl = 'input_cov_mcfs_10m.pkl'
+mcfs_pkl = 'input_cov_xattr_mcfs_10m.pkl'
 iocov_mcfs_input_cnt = get_syscall_count_from_pkl(mcfs_pkl)
 
 mcfs_log_input_cnt = {}
@@ -25,10 +25,10 @@ for seq_path in mcfs_seqs:
 print('mcfs_log_input_cnt: ', mcfs_log_input_cnt)
 print('iocov_mcfs_input_cnt: ', iocov_mcfs_input_cnt)
 
-iocov_mcfs_accuracy = {}
+iocov_mcfs_precision = {}
 
 for sc in INPUT_SYSCALLS.keys():
     if mcfs_log_input_cnt[sc] != 0:
-        iocov_mcfs_accuracy[sc] = str((1 - abs(mcfs_log_input_cnt[sc] - iocov_mcfs_input_cnt[sc]) / mcfs_log_input_cnt[sc]) * 100 ) + ' %'
+        iocov_mcfs_precision[sc] = str((1 - abs(mcfs_log_input_cnt[sc] - iocov_mcfs_input_cnt[sc]) / mcfs_log_input_cnt[sc]) * 100 ) + ' %'
 
-print('iocov_mcfs_accuracy: ', iocov_mcfs_accuracy)
+print('iocov_mcfs_precision: ', iocov_mcfs_precision)
