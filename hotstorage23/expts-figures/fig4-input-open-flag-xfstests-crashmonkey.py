a56b80fdb5e10b044b5c081adf2718f7b6ab2904
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-03-14 17:45:38 -0400

    RMSD calculation

--- hotstorage23/expts-figures/fig4-input-open-flag-xfstests-crashmonkey.py
+++ hotstorage23/expts-figures/fig4-input-open-flag-xfstests-crashmonkey.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import statistics
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pickle
+from scipy.stats import gmean
+import math
 
 plt.rcParams["font.family"] = "Times New Roman"
 
@@ -82,3 +85,52 @@ plt.tight_layout()
 
 # Save the plot to a PDF file as a vector plot
 plt.savefig('fig4-open-flag-xfstests-crashmonkey.pdf', format='pdf', bbox_inches='tight')
+
+
+# print('data1_xfstests: ', data1_xfstests)
+# print('data2_crashmonkey: ', data2_crashmonkey)
+
+def safe_log(x):
+    """
+    Calculate the logarithm of a positive number or return 0 for the logarithm of 0.
+    """
+    if x > 0:
+        return math.log2(x)
+    else:
+        return 0
+
+data1_xfstests = [safe_log(x) for x in data1_xfstests]
+data2_crashmonkey = [safe_log(x) for x in data2_crashmonkey]
+
+xfstests_avg = statistics.mean(data1_xfstests)
+crashmonkey_avg = statistics.mean(data2_crashmonkey)
+
+xfstests_avg_list = [xfstests_avg] * len(data1_xfstests)
+crashmonkey_avg_list = [crashmonkey_avg] * len(data2_crashmonkey)
+
+def rmsd(actual, predicted):
+    """
+    Calculate the Root Mean Square Deviation (RMSD) between two lists.
+    """
+    # Check that the two lists have the same length
+    if len(actual) != len(predicted):
+        raise ValueError("The two lists must have the same length.")
+
+    # Calculate the squared differences between the actual and predicted values
+    squared_differences = [(actual[i] - predicted[i]) ** 2 for i in range(len(actual))]
+
+    # Calculate the mean squared difference
+    mean_squared_difference = sum(squared_differences) / len(actual)
+
+    # Calculate the RMSD by taking the square root of the mean squared difference
+    rmsd = math.sqrt(mean_squared_difference)
+
+    return rmsd
+
+
+rmsd_xfstests = rmsd(xfstests_avg_list, data1_xfstests)
+
+rmsd_crashmonkey = rmsd(crashmonkey_avg_list, data2_crashmonkey)
+
+print('rmsd_xfstests: ', rmsd_xfstests)
+print('rmsd_crashmonkey: ', rmsd_crashmonkey)
