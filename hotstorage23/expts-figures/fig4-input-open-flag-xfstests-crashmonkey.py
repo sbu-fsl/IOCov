234a25739b6f75965b91893f2c0c6327f641ed3f
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-03-17 16:11:06 -0400

    Improve figures for hotstorage23

--- hotstorage23/expts-figures/fig4-input-open-flag-xfstests-crashmonkey.py
+++ hotstorage23/expts-figures/fig4-input-open-flag-xfstests-crashmonkey.py
@@ -68,8 +68,8 @@ ax.set_xlim(xmin = 0.1)
 
 # Create the stacked bar chart
 # 'green' 'orange'
-ax.barh(x_labels, data[0], color='#4daf4a', label='CrashMonkey')
-ax.barh(x_labels, data[1], color='#ff7f0e', left=data[0], hatch='////', label='xfstests')
+ax.barh(x_labels, data[0], color='#4daf4a', edgecolor='black', linewidth=0.5, label='CrashMonkey')
+ax.barh(x_labels, data[1], color='#ff7f0e',  edgecolor='black', linewidth=0.5, left=data[0], hatch='////', label='xfstests')
 
 
 # Add a title and axis labels
@@ -84,72 +84,4 @@ ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=len(labels))
 plt.tight_layout()
 
 # Save the plot to a PDF file as a vector plot
-plt.savefig('fig4-open-flag-xfstests-crashmonkey.pdf', format='pdf', bbox_inches='tight')
-
-
-# print('data1_xfstests: ', data1_xfstests)
-# print('data2_crashmonkey: ', data2_crashmonkey)
-
-def safe_log(x):
-    """
-    Calculate the logarithm of a positive number or return 0 for the logarithm of 0.
-    """
-    if x > 0:
-        return math.log2(x)
-    else:
-        return 0
-
-def min_max_normalize(lst):
-    min_val = min(lst)
-    max_val = max(lst)
-    normalized_lst = [(x - min_val) / (max_val - min_val) for x in lst]
-    return normalized_lst
-
-def safe_geometric_mean(numbers):
-    product = 1
-    for num in numbers:
-        if num != 0:
-            product *= num
-    return pow(product, 1/len(numbers))
-
-data1_xfstests = min_max_normalize(data1_xfstests)
-data2_crashmonkey = min_max_normalize(data2_crashmonkey)
-
-# data1_xfstests = [safe_log(x) for x in data1_xfstests]
-# data2_crashmonkey = [safe_log(x) for x in data2_crashmonkey]
-
-# xfstests_avg = statistics.mean(data1_xfstests)
-# crashmonkey_avg = statistics.mean(data2_crashmonkey)
-
-xfstests_avg = safe_geometric_mean(data1_xfstests)
-crashmonkey_avg = safe_geometric_mean(data2_crashmonkey)
-
-xfstests_avg_list = [xfstests_avg] * len(data1_xfstests)
-crashmonkey_avg_list = [crashmonkey_avg] * len(data2_crashmonkey)
-
-def rmsd(actual, predicted):
-    """
-    Calculate the Root Mean Square Deviation (RMSD) between two lists.
-    """
-    # Check that the two lists have the same length
-    if len(actual) != len(predicted):
-        raise ValueError("The two lists must have the same length.")
-
-    # Calculate the squared differences between the actual and predicted values
-    squared_differences = [(actual[i] - predicted[i]) ** 2 for i in range(len(actual))]
-
-    # Calculate the mean squared difference
-    mean_squared_difference = sum(squared_differences) / len(actual)
-
-    # Calculate the RMSD by taking the square root of the mean squared difference
-    rmsd = math.sqrt(mean_squared_difference)
-
-    return rmsd
-
-
-rmsd_xfstests = rmsd(xfstests_avg_list, data1_xfstests)
-
-rmsd_crashmonkey = rmsd(crashmonkey_avg_list, data2_crashmonkey)
-
-print('rmsd_xfstests: ', rmsd_xfstests)
-print('rmsd_crashmonkey: ', rmsd_crashmonkey)
+plt.savefig('fig4-open-flags.pdf', format='pdf', bbox_inches='tight')
--- hotstorage23/expts-figures/fig5-input-write-count-xfstests.py
+++ hotstorage23/expts-figures/fig5-input-write-count-xfstests.py
@@ -12,69 +12,98 @@ plt.rcParams["font.family"] = "Times New Roman"
 
 pkl_dir = os.getcwd()
 
-fig5_xfstests_input_coords = {}
+def read_write_count_by_pkl(pkl_file):
+    fig5_xfstests_input_coords = {}
 
-with open(os.path.join(pkl_dir, 'fig5_xfstests_input_coords.pkl'), 'rb') as f:
-    fig5_xfstests_input_coords = pickle.load(f)
+    with open(os.path.join(pkl_dir, pkl_file), 'rb') as f:
+        fig5_xfstests_input_coords = pickle.load(f)
 
-xfstests_write_count = fig5_xfstests_input_coords['write']['count']
+    xfstests_write_count = fig5_xfstests_input_coords['write']['count']
 
-X_xfstests = xfstests_write_count['X-axis']
-Y_xfstests = xfstests_write_count['Y-axis']
+    X_xfstests = xfstests_write_count['X-axis']
+    Y_xfstests = xfstests_write_count['Y-axis']
 
-# print('xfstests_write_count: ', xfstests_write_count)
+    # print('xfstests_write_count: ', xfstests_write_count)
+    """
+    print('len(X_xfstests): ', len(X_xfstests))
+    for i in range(len(X_xfstests) - 1, -1, -1):
+        if Y_xfstests[i] > 0:
+            print('i: ', i)
+            print('X_xfstests: ', X_xfstests[i])
+            break
+    """
+    Keep = -1
+    for i in range(len(X_xfstests)):
+        each_X = X_xfstests[i]
+        if each_X == '2^32':
+            Keep = i 
 
-"""
-print('len(X_xfstests): ', len(X_xfstests))
-for i in range(len(X_xfstests) - 1, -1, -1):
-    if Y_xfstests[i] > 0:
-        print('i: ', i)
-        print('X_xfstests: ', X_xfstests[i])
-        break
-"""
+    # Labels
+    X_cut_xfstests = X_xfstests[0:Keep+1]
+    # Real Values
+    Y_cut_xfstests = Y_xfstests[0:Keep+1]
 
-Keep = -1
-for i in range(len(X_xfstests)):
-    each_X = X_xfstests[i]
-    if each_X == '2^32':
-        Keep = i 
+    X_xfstests = []
+    Y_xfstests = []
+    for i in range(len(X_cut_xfstests)):
+        if X_cut_xfstests[i] != 'Intv.':
+            X_xfstests.append(X_cut_xfstests[i])
+            Y_xfstests.append(Y_cut_xfstests[i])
 
-# Labels
-X_cut_xfstests = X_xfstests[0:Keep+1]
-# Real Values
-Y_cut_xfstests = Y_xfstests[0:Keep+1]
+    X_xfstests.pop(0)
+    Y_xfstests.pop(0)
+    # print('X_xfstests: ', X_xfstests)
+    # print('Y_xfstests: ', Y_xfstests)
 
-X_xfstests = []
-Y_xfstests = []
-for i in range(len(X_cut_xfstests)):
-    if X_cut_xfstests[i] != 'Intv.':
-        X_xfstests.append(X_cut_xfstests[i])
-        Y_xfstests.append(Y_cut_xfstests[i])
+    # Do not use 2^10, use 10 instead 
+    for i in range(len(X_xfstests)):
+        X_xfstests[i] = X_xfstests[i].split('^')[-1]
+    return X_xfstests, Y_xfstests
 
-X_xfstests.pop(0)
-Y_xfstests.pop(0)
-print('X_xfstests: ', X_xfstests)
-print('Y_xfstests: ', Y_xfstests)
 
-# Do not use 2^10, use 10 instead 
-for i in range(len(X_xfstests)):
-    X_xfstests[i] = X_xfstests[i].split('^')[-1]
+xfstests_pkl_file = 'fig5_xfstests_input_coords.pkl'
+crashmonkey_pkl_file = 'fig5_crashmonkey_input_coords.pkl'
+
+X_xfstests, Y_xfstests = read_write_count_by_pkl(xfstests_pkl_file)
+X_crashmonkey, Y_crashmonkey = read_write_count_by_pkl(crashmonkey_pkl_file)
 
 # print('X_xfstests: ', X_xfstests)
 # print('Y_xfstests: ', Y_xfstests)
 
+# print('X_crashmonkey: ', X_crashmonkey)
+# print('Y_crashmonkey: ', Y_crashmonkey)
+
+for i in range(len(X_xfstests)):
+    if Y_xfstests[i] < Y_crashmonkey[i]:
+        print('xfstests is SMALLER than crashmonkey')
+
+Y_diff = []
+for i in range(len(X_xfstests)):
+    Y_diff.append(Y_xfstests[i] - Y_crashmonkey[i])
+
+Y_data = np.array([Y_crashmonkey, Y_diff])
+
+labels = ['CrashMonkey', 'xfstests']
+
 ### Plotting 
-fig, ax = plt.subplots(figsize=(10, 6))
+# fig, ax = plt.subplots(figsize=(10, 6))
+fig, ax = plt.subplots()
 
 y_pos = np.arange(len(X_xfstests))
 plt.yticks(y_pos, X_xfstests)
+
+ax.set_xscale('log')
 
 xtick_values = [0.1, 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000]
 # xtick_labels = ['0', '1', '2', '3 (1K)', '4', '5', '6 (1M)', '7 (10M)']
 xtick_labels = ['0', '1', '10', '100', '1K', '10K', '100K', '1M', '10M']
 
 # 'maroon', ['#77b5e5' (blue), '#4daf4a' (green), '#f0e442' (yellow), '#d62728' (red), '#984ea3' (purple), '#ff7f0e' (orange)]
-ax.barh(y_pos, Y_xfstests, log=True, color ='#77b5e5', edgecolor='black', linewidth=0.5)
+# ax.barh(y_pos, Y_xfstests, log=True, color ='#77b5e5', edgecolor='black', linewidth=0.5)
+
+ax.barh(y_pos, Y_data[0], color='#4daf4a', edgecolor='black', linewidth=0.5, label='CrashMonkey')
+ax.barh(y_pos, Y_data[1], color='#ff7f0e',  edgecolor='black', linewidth=0.5, left=Y_data[0], hatch='////', label='xfstests')
+
 # plt.xlim(left=0.1)
 
 ax.set_yticks(ax.get_yticks()[::2])
@@ -83,6 +112,7 @@ plt.xticks(xtick_values, xtick_labels)
 
 ax.set_xlim(xmin = 0.1)
 
+"""
 # Set the arrow
 arrow_index = 26
 arrow_x = Y_xfstests[arrow_index]
@@ -90,10 +120,17 @@ arrow_y = y_pos[arrow_index]
 arrow_text = f'Max write size 258 MiB'
 ax.annotate(arrow_text, xy=(arrow_x, arrow_y), xytext=(arrow_x+5, arrow_y),
              arrowprops=dict(facecolor='red', arrowstyle='->'))
+"""
 
 #ax.set_title('My Bar Chart')
 ax.set_xlabel('Frequency (log scale base 10)', fontweight='bold')
 ax.set_ylabel('Write Size in Bytes (exponent of log base 2)', fontweight='bold')
 
+# Add a legend
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=len(labels))
+
+# Adjust the plot layout
+plt.tight_layout()
+
 # dpi=dpi_val
-fig.savefig('fig5-write-count-xfstests.pdf', format='pdf', bbox_inches='tight')
+fig.savefig('fig5-write-count.pdf', format='pdf', bbox_inches='tight')
