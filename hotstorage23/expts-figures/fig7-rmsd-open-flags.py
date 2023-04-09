fca4411e22c1df073e57e904089fce73571ee2e1
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-04-09 17:08:02 -0400

    fig7

--- hotstorage23/expts-figures/fig7-rmsd-open-flags.py
+++ hotstorage23/expts-figures/fig7-rmsd-open-flags.py
@@ -120,7 +120,7 @@ ax.legend(loc='best', fontsize=5)
 # Add a title and axis labels
 # plt.title('Line Plot Example')
 ax.set_xlabel('Target Value T (log scale base 10)', fontweight='bold')
-ax.set_ylabel('RMSD Value', fontweight='bold')
+ax.set_ylabel('TCA Value', fontweight='bold')
 
 ax.set_axisbelow(True)
 ax.grid(axis='y', linestyle='-', alpha=0.3)
