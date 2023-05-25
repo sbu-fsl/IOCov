9775cc5cb090f1c708349c4f46dfd2e963ab86ef
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-05-24 23:02:33 -0400

    copy original file to crc-fig4-input-open-flags.py

---
+++ hotstorage23/expts-figures/crc-fig4-input-open-flags.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import statistics
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pickle
+from scipy.stats import gmean
+import math
+
+plt.rcParams["font.family"] = "Times New Roman"
+
+# pkl_dir = '/mcfs2/LTTng-xfstests-2022-1211/IOCov/src'
+pkl_dir = os.getcwd()
+
+fig4_xfstests_input = {}
+fig4_crashmonkey_input = {}
+dpi_val = 600
+
+with open(os.path.join(pkl_dir, 'fig4_input_cov_all_xfstests_xattrs.pkl'), 'rb') as f:
+    fig4_xfstests_input = pickle.load(f)
+
+with open(os.path.join(pkl_dir, 'fig4_input_cov_crashmonkey.pkl'), 'rb') as f:
+    fig4_crashmonkey_input = pickle.load(f)
+
+fig4_xfstests_open_flags = fig4_xfstests_input['open']['flags']
+fig4_crashmonkey_open_flags = fig4_crashmonkey_input['open']['flags']
+
+# print('fig4_xfstests_open_flags: ', fig4_xfstests_open_flags)
+# print('fig4_crashmonkey_open_flags: ', fig4_crashmonkey_open_flags)
+
+data1_xfstests = []
+data2_crashmonkey = []
+data3_diff = []
+
+x_labels = []
+
+# It is true for open flags
+crashmonkey_subset_xfstests = True 
+
+for open_flag in sorted(fig4_xfstests_open_flags.keys(), reverse=True):
+    x_labels.append(open_flag)
+    if fig4_xfstests_open_flags[open_flag] < fig4_crashmonkey_open_flags[open_flag]:
+        crashmonkey_subset_xfstests = False
+    data1_xfstests.append(fig4_xfstests_open_flags[open_flag])
+    data2_crashmonkey.append(fig4_crashmonkey_open_flags[open_flag])
+    data3_diff.append(fig4_xfstests_open_flags[open_flag] - fig4_crashmonkey_open_flags[open_flag])
+
+data = np.array([data2_crashmonkey, data3_diff])
+
+labels = ['CrashMonkey', 'xfstests']
+# Set up the plot
+fig, ax = plt.subplots()
+
+ax.set_xscale('log')
+
+# set the tick locations and labels on the x-axis
+# xtick_values = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000]
+# xtick_labels = ['1', '10', '100', '1000', '10000', '100000', '1000000', '10000000']
+
+xtick_values = [0.1, 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000]
+# xtick_labels = ['0', '1', '2', '3 (1K)', '4', '5', '6 (1M)', '7 (10M)']
+xtick_labels = ['0', '1', '10', '100', '1K', '10K', '100K', '1M', '10M']
+
+plt.xticks(xtick_values, xtick_labels)
+
+ax.set_xlim(xmin = 0.1)
+
+# Create the stacked bar chart
+# 'green' 'orange'
+ax.barh(x_labels, data[0], color='#4daf4a', edgecolor='black', linewidth=0.5, label='CrashMonkey')
+ax.barh(x_labels, data[1], color='#ff7f0e',  edgecolor='black', linewidth=0.5, left=data[0], hatch='////', label='xfstests')
+
+
+# Add a title and axis labels
+# ax.set_title('Stacked Bar Chart')
+ax.set_xlabel('Frequency (log scale base 10)', fontweight='bold')
+ax.set_ylabel('Open Flags', fontweight='bold')
+
+# Add a legend
+# ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=len(labels))
+ax.legend(loc='best', ncol=len(labels))
+
+ax.set_axisbelow(True)
+ax.grid(axis='x', linestyle='-', alpha=0.3)
+
+# Adjust the plot layout
+plt.tight_layout()
+
+# Save the plot to a PDF file as a vector plot
+plt.savefig('crc-input-open-flags.pdf', format='pdf', bbox_inches='tight')
