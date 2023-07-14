9c5170df0c7849fcff3b0102701b1951d2aef324
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-07-14 17:40:52 +0000

    Ignore xlsx files

--- Syzkaller/syscall_parse.py
+++ Syzkaller/syscall_parse.py
@@ -7,16 +7,22 @@ workbook = openpyxl.Workbook()
 
 # function to get the arguments
 def between_brackets(str):
-    index1 = str.find("(")
-    index2 = str.rfind(")")
+    index1 = str.find('(')
+    index2 = str.rfind(')')
 
     return str[:index1], str[index1+1:index2]
-directory = '/mnt/c/Users/Rohan/html'
+directory = '/home/ubuntu/syzkaller/yf-syz-webdump-2023-0706/Syzwebs-18mins-2023-0707-0410/only-inputs'
 
 mydict = {}
 
-# We are focusing on syscalls related to file systems for now
-syscalls = ["open","openat","creat","openat2","read","pread64","write","pwrite64","lseek","llseek","truncate","ftruncate","mkdir","mkdirat","chmod","fchmod","fchmodat","close","close_range","chdir","fchdir","setxattr","lsetxattr","fsetxattr","getxattr","lgetxattr","fgetxattr","read"]
+# We are focusing on syscalls related to file systems and supported by IOCov for now
+syscalls = ['open', 'openat', 'creat', 'openat2', 
+            'read', 'pread64', 'write', 'pwrite64',
+            'lseek', 'llseek', 'truncate', 'ftruncate', 
+            'mkdir', 'mkdirat', 'chmod', 'fchmod', 'fchmodat', 
+            'close', 'close_range', 'chdir', 'fchdir', 
+            'setxattr', 'lsetxattr', 'fsetxattr', 
+            'getxattr', 'lgetxattr', 'fgetxattr']
 
 for call in syscalls:
     mydict[call] = [[]]
@@ -32,7 +38,7 @@ for filename in os.listdir(directory):
             # Parse Syzkaller syscall sequence
             call_str, rest_str = between_brackets(line)
 
-            # If the call_str matches a call in the "syscalls"
+            # If the call_str matches a call in the 'syscalls'
             if call in call_str:
                 # Append syscall name and all the arguments as a sub-list
                 mydict[call].append([call_str] + rest_str.split(','))
