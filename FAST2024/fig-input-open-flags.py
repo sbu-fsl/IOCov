60acc6903307c0682bd0de9d0ae84aacb0e49266
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-08-13 22:49:34 -0400

    Refine fig-input-open-flags FAST24

--- FAST2024/fig-input-open-flags.py
+++ FAST2024/fig-input-open-flags.py
@@ -15,46 +15,26 @@ plt.rcParams["font.family"] = "Times New Roman"
 dpi_val = 600
 
 pkl_dir = '/mcfs/iocov-mcfs-fast24-2023-0723/IOCov/FAST2024/input-pickles'
+figure_dir = '/mcfs/iocov-mcfs-fast24-2023-0723/IOCov/FAST2024/expts-figures'
+figure_file_name = 'fast24-input-open-flags.pdf'
 
-xfstests_input = {}
-crashmonkey_input = {}
-syzkaller_input = {}
-# Uniform or prob open inputs
-mcfs_uniform_input = {}
-mcfs_prob_input = {}
-mcfs_prob_inverse_input = {}
+all_open_flags = []
+pkl_files = [
+    'fig4_input_cov_all_xfstests_xattrs.pkl',
+    'fig4_input_cov_crashmonkey.pkl',
+    'input_cov_syzkaller_40mins_2023_0809_0037.pkl',
+    'input_cov_mcfs_Uniform_50p_40mins_open_flags_20230810_170456_382883.pkl', # Uniform or prob open inputs
+    'input_cov_mcfs_Prob_5factor_40mins_open_flags_20230810_181953_484817.pkl',
+    'input_cov_mcfs_Inverse_Prob_5factor_40mins_sequence_pan_20230810_190452_580771.pkl'
+    ]
+num_tools = len(pkl_files)
+for i in range(num_tools):
+    with open(os.path.join(pkl_dir, pkl_files[i]), 'rb') as f:
+        input_data = pickle.load(f)
+        all_open_flags.append(input_data['open']['flags'])
 
-with open(os.path.join(pkl_dir, 'fig4_input_cov_all_xfstests_xattrs.pkl'), 'rb') as f:
-    xfstests_input = pickle.load(f)
 
-with open(os.path.join(pkl_dir, 'fig4_input_cov_crashmonkey.pkl'), 'rb') as f:
-    crashmonkey_input = pickle.load(f)
-
-with open(os.path.join(pkl_dir, 'input_cov_syzkaller_40mins_2023_0809_0037.pkl'), 'rb') as f:
-    syzkaller_input = pickle.load(f)
-
-with open(os.path.join(pkl_dir, 'input_cov_mcfs_Uniform_50p_40mins_open_flags_20230810_170456_382883.pkl'), 'rb') as f:
-    mcfs_uniform_input = pickle.load(f)
-
-with open(os.path.join(pkl_dir, 'input_cov_mcfs_Prob_5factor_40mins_open_flags_20230810_181953_484817.pkl'), 'rb') as f:
-    mcfs_prob_input = pickle.load(f)
-
-with open(os.path.join(pkl_dir, 'input_cov_mcfs_Inverse_Prob_5factor_40mins_sequence_pan_20230810_190452_580771.pkl'), 'rb') as f:
-    mcfs_prob_inverse_input = pickle.load(f)
-
-xfstests_open_flags = xfstests_input['open']['flags']
-crashmonkey_open_flags = crashmonkey_input['open']['flags']
-syzkaller_open_flags = syzkaller_input['open']['flags']
-mcfs_uniform_open_flags = mcfs_uniform_input['open']['flags']
-mcfs_prob_open_flags = mcfs_prob_input['open']['flags']
-mcfs_prob_inverse_open_flags = mcfs_prob_inverse_input['open']['flags']
-
-data1_crashmonkey = []
-data2_xfstests = []
-data3_syzkaller = []
-data4_mcfs_uniform = []
-data5_mcfs_prob = []
-data6_mcfs_prob_inverse = []
+# print('all_open_flags: ', all_open_flags)
 
 x_labels = []
 
@@ -63,25 +43,29 @@ x_labels = []
 
 ignored_flags = ['O_ACCMODE', 'O_RDONLY']
 
-for open_flag in sorted(xfstests_open_flags.keys()):
+# Init a list which includes num_tools sub lists [ []*num_tools ]
+# Have to use this way to create empty sublists
+all_data = [[] for _ in range(6)]
+
+# For each specific open flag
+for open_flag in sorted(all_open_flags[0].keys()):
     if open_flag not in ignored_flags:
         x_labels.append(open_flag)
-        data1_crashmonkey.append(crashmonkey_open_flags[open_flag])
-        data2_xfstests.append(xfstests_open_flags[open_flag])
-        data3_syzkaller.append(syzkaller_open_flags[open_flag])
-        data4_mcfs_uniform.append(mcfs_uniform_open_flags[open_flag])
-        data5_mcfs_prob.append(mcfs_prob_open_flags[open_flag])
-        data6_mcfs_prob_inverse.append(mcfs_prob_inverse_open_flags[open_flag])
+        for i in range(num_tools):
+            all_data[i].append(all_open_flags[i][open_flag])
 
-data = np.array([data1_crashmonkey, data2_xfstests, data3_syzkaller, data4_mcfs_uniform, data5_mcfs_prob, data6_mcfs_prob_inverse])
+all_data_arr = np.array(all_data)
+
+# print('x_labels: ', x_labels)
+# print('all_data: ', all_data)
+# print('all_data_arr: ', all_data_arr)
+# print('===============================')
 
 # Numbers of open flags (x ticks)
-N_open_flags = len(xfstests_open_flags.keys()) - len(ignored_flags)
+N_open_flags = len(all_open_flags[0].keys()) - len(ignored_flags)
 
 # Position of bars on x-axis
 x_pos = np.arange(N_open_flags)
-
-labels = ['CrashMonkey', 'xfstests', 'Syzkaller', 'MCFS-Uniform', 'MCFS-Prob', 'MCFS-Prob-Inv']
 
 # Determine the width and height in inches
 width_inches = 9  # Example width for a two-column area
@@ -102,18 +86,23 @@ plt.yticks(ytick_values, ytick_labels)
 
 ax.set_ylim(ymin = 0.1)
 
+# print('all_data_arr: ', all_data_arr)
 
-ax.bar(x_pos - 5 * width / 2, data[0], width, color='#1f77b4', edgecolor='black', linewidth=0.5, label='CrashMonkey')
-ax.bar(x_pos - 3 * width / 2, data[1], width, color='#ff7f0e',  edgecolor='black', linewidth=0.5, label='xfstests')
-ax.bar(x_pos - width / 2, data[2], width, color='#2ca02c',  edgecolor='black', linewidth=0.5, label='Syzkaller')
-ax.bar(x_pos + width / 2, data[3], width, color='#d62728',  edgecolor='black', linewidth=0.5, label='MCFS Uniform')
-ax.bar(x_pos + 3 * width / 2, data[4], width, color='#9467bd',  edgecolor='black', linewidth=0.5, label='MCFS Prob')
-ax.bar(x_pos + 5 * width / 2, data[5], width, color='#8c564b',  edgecolor='black', linewidth=0.5, label='MCFS Prob Inv')
+# Plot the data
+bar_coords = [x_pos - 5 * width / 2, x_pos - 3 * width / 2, x_pos - width / 2, x_pos + width / 2, x_pos + 3 * width / 2, x_pos + 5 * width / 2]
+bar_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b']
+edgecolors = ['black', 'black', 'black', 'black', 'black', 'black']
+linewidths = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+labels = ['CrashMonkey', 'xfstests', 'Syzkaller', 'MCFS Uniform', 'MCFS Prob', 'MCFS Prob Inv']
+
+for i in range(num_tools):
+    ax.bar(bar_coords[i], all_data_arr[i], width, color=bar_colors[i], edgecolor=edgecolors[i], linewidth=linewidths[i], label=labels[i])
 
 ax.set_ylabel('Frequency (log scale base 10)', fontweight='bold')
 ax.set_xlabel('Open Flags', fontweight='bold')
 
-ax.legend(loc='best', ncol=len(labels))
+# ax.legend(loc='best', ncol=len(labels))
+ax.legend(fontsize='small', loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=len(labels))
 
 ax.set_axisbelow(True)
 ax.grid(axis='y', linestyle='-', alpha=0.3)
@@ -127,4 +116,4 @@ plt.xticks(x_pos + width / 2, x_labels, rotation=45, ha='right', fontsize=8)
 plt.tight_layout()
 
 # Save the plot to a PDF file as a vector plot
-plt.savefig('fast24-input-open-flags.pdf', format='pdf', bbox_inches='tight')
+plt.savefig(os.path.join(figure_dir, figure_file_name), format='pdf', bbox_inches='tight')
--- FAST2024/read-pickles.py
+++ FAST2024/read-pickles.py
@@ -12,7 +12,9 @@ plk_files = ['fig4_input_cov_all_xfstests_xattrs.pkl',
              'syzkaller_40mins_2023_0809_0037_input_coords.pkl',
              'input_cov_mcfs_Uniform_50p_40mins_open_flags_20230810_170456_382883.pkl',
              'input_cov_mcfs_Prob_5factor_40mins_open_flags_20230810_181953_484817.pkl',
-             'input_cov_mcfs_Inverse_Prob_5factor_40mins_sequence_pan_20230810_190452_580771.pkl']
+             'input_cov_mcfs_Inverse_Prob_5factor_40mins_sequence_pan_20230810_190452_580771.pkl',
+             'input_cov_mcfs_Uniform_40mins_write_sizes_20230812_213410_786070.pkl',
+             'mcfs_Uniform_40mins_write_sizes_20230812_213410_786070_input_coords.pkl']
 
 plk_dir = '/mcfs/iocov-mcfs-fast24-2023-0723/IOCov/FAST2024/input-pickles'
 
