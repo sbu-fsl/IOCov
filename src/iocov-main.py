25637906d02a92f0afe98d91a4690978424df5f5
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-11-07 16:54:32 -0500

    Add comments to iocov-main.py

--- src/iocov-main.py
+++ src/iocov-main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-# import LTTng parser to parse LTTng output logs
+"""
+The main entry to parse LTTng logs and plot input and output coverage
+"""
+
+# Import LTTng parser to parse LTTng output logs
 from parser import *
-# Plot input/output coverage
+# Import plotter to plot input/output coverage
 from plotter import *
 import pickle 
 import time
@@ -12,45 +16,64 @@ import sys
 import os
 
 def main(args):
+    """Main function to accept arguments and call parser and plotter"""
+
     # Fetch arguments
     need_parse = args.parse
     name_suffix = args.suffix
     is_mcfs = args.mcfs
-    # define empty input/output coverage dict
+    # Define empty input/output coverage dict to be filled by parser or read from pickle files
     input_cov = {}
     output_cov = {}
     unfilter_input_cov = {}
+
+    # If we need to parse LTTng logs.  If we already have pickle files, we don't need to parse again.
     if need_parse:
         file_path = args.filepath
+        # Check if pickle files already exist; if yes, exit
         if (os.path.exists('input-cov-{}.pkl'.format(name_suffix)) or 
             os.path.exists('output-cov-{}.pkl'.format(name_suffix)) or 
             os.path.exists('unfilter-input-cov-{}.pkl'.format(name_suffix))):
             sys.exit('Cov pickle files already exist.')
-        # tic = time.perf_counter()
+        
+        # Parse LTTng logs and get input/output coverage by calling TraceParser
         tp = TraceParser(file_path, is_mcfs, name_suffix)
         input_cov, output_cov, unfilter_input_cov = tp.cal_input_output_cov()
-        # toc = time.perf_counter()
-        # print(f"LTTng analyzer completed in {toc - tic:0.4f} seconds")
+        
+        # Write input/output cov to pickle files
         with open('input-cov-{}.pkl'.format(name_suffix), 'wb') as f:
             pickle.dump(input_cov, f)
         with open('output-cov-{}.pkl'.format(name_suffix), 'wb') as f:
             pickle.dump(output_cov, f)
         with open('unfilter-input-cov-{}.pkl'.format(name_suffix), 'wb') as f:
             pickle.dump(unfilter_input_cov, f)
+    
+    # If we don't need to parse LTTng logs, we only need to read pickle files
+    # Here, we provide different plotting options
     else:
+        # Fetch arguments
         need_json = args.json
         need_plot = args.plot
         input_only = args.ploti
         output_only = args.ploto
+
+        # Read input/output cov from pickle files
         with open('input-cov-{}.pkl'.format(name_suffix), 'rb') as f:
             input_cov = pickle.load(f)
+        
         # TODO: double-check if this workaround is sufficient 
+        # If we are not plotting input coverage only, we need to read output coverage
         if not input_only:
             with open('output-cov-{}.pkl'.format(name_suffix), 'rb') as f:
                 output_cov = pickle.load(f)
+        
+        # TODO: we have to read unfilter-input-cov-{}.pkl any time while plotting
+        #       but this is not necessary. We only read it when we need to plot 
+        #       unfiltered input coverage 
         with open('unfilter-input-cov-{}.pkl'.format(name_suffix), 'rb') as f:
             unfilter_input_cov = pickle.load(f)
-        # Write input/output cov to json files if needed
+        
+        # Write input/output cov to json files for better readability if needed
         if need_json:
             with open('input-cov-{}.json'.format(name_suffix), 'w') as fout:
                 input_cov_str = json.dumps(input_cov, indent=4)
@@ -61,11 +84,16 @@ def main(args):
             with open('unfilter-input-cov-{}.json'.format(name_suffix), 'w') as fout:
                 unfilter_input_cov_str = json.dumps(unfilter_input_cov, indent=4)
                 print(unfilter_input_cov_str, file=fout)
+        
+        # Plot input/output coverage if needed
         if need_plot:
+            # Passing argumetns to create TracePlotter object to plot input/output coverage
             plot_dir = args.plotdir
             plot_title =  args.plottitle
             plot_unfilter = args.plotunfilter
             tplot = TracePlotter(plot_dir, plot_title, plot_unfilter, input_cov, output_cov, unfilter_input_cov)
+
+            # Plot corresponding input/output coverage based on arguments
             if input_only:
                 tplot.plot_inputs()
             if output_only:
@@ -75,56 +103,68 @@ def main(args):
                 tplot.plot_outputs()
 
 if __name__ == "__main__":
-    cwd = os.getcwd()
     """
-        IMPORTANT: Need to edit find_testing_filename AND default_lttng_log path AND default_plot_name AND default_is_mcfs
-    """
-    # Handle Arguments
-    # Example commands:
+    IMPORTANT: Need to edit find_testing_filename AND default_lttng_log path AND default_plot_name AND default_is_mcfs
+
+    ################### Example usage commands ###################
     ## Parse LTTng logs and get pickle files for input/output cov only
-    #### python3 iocov-main.py default_plot_name --parse
+    python3 iocov-main.py <default_plot_name> --parse
+
     ## When already has i/o cov pickle files, generate json files (without plotting)
-    #### python3 iocov-main.py default_plot_name --no-parse --json
+    python3 iocov-main.py <default_plot_name> --no-parse --json
+
     ## When already has i/o cov pickle files, plot without considering unfiltered input coverage
-    #### python3 iocov-main.py default_plot_name --no-parse --plot
+    python3 iocov-main.py <default_plot_name> --no-parse --plot
+
     ## When already has i/o cov pickle files, plot with considering unfiltered input coverage
-    #### python3 iocov-main.py default_plot_name --no-parse --plot --plotunfilter
+    python3 iocov-main.py <default_plot_name> --no-parse --plot --plotunfilter
+
     ## Plot input coverage only
-    #### python3 iocov-main.py default_plot_name --no-parse --plot -i
+    python3 iocov-main.py <default_plot_name> --no-parse --plot -i
+
     ## Plot output coverage only
-    #### python3 iocov-main.py default_plot_name --no-parse --plot -o
-    
-    ## Crashmonkey example
-    # default_plot_name = 'crashmonkey'
-    # default_is_mcfs = False
-    # default_lttng_log = 'crashmonkey-lttng-ext4-allrecur-614.log'
+    python3 iocov-main.py <default_plot_name> --no-parse --plot -o
+
+    ################### Example arguments to set ###################
+    ## Use hyphen ("-") in file names, not underscore ("_"), otherwise 
+    ## some pathname parsing will not work properly
+
+    ## CrashMonkey example
+    default_plot_name = 'crashmonkey'
+    default_is_mcfs = False
+    default_lttng_log = 'crashmonkey-lttng-ext4-allrecur-614.log'
 
     ## Syzkaller example
-    # default_plot_name = 'syzkaller_26hours_2023_0708_0548'
+    default_plot_name = 'syzkaller-26hours-2023-0708-0548'
 
     ## Xfstests example
-    # default_plot_name = 'xfstests_xattr_open_dump'
+    default_plot_name = 'xfstests-xattr-open-dump'
 
-    ## MCFS example LATEST default_plot_name
-    # default_plot_name = 'mcfs_Uniform_40mins_write_sizes_20230812_213410_786070'
-    default_plot_name_parts = sys.argv[1].split('.')[0].split('-')[2:]
-    default_plot_name = '-'.join(default_plot_name_parts)
+    ## MCFS example
+    default_plot_name = 'mcfs-Uniform-40mins-write-sizes-20230812-213410-786070'
+    """
 
-    # print('default_plot_name: ', default_plot_name)
-    # print('type(default_plot_name): ', default_plot_name)
-
-    # For computing IOCov accuracy
-    # default_plot_name = 'xfstests_xattr_open_dump'
+    # Default values for arguments to manually set
+    default_plot_name = 'xfstests-xattr-open-dump'
     default_is_mcfs = False
     default_lttng_log = 'xfstests-lttng-all-related-ext4-all-xattrs-4633.log'
 
+    ## For some cases when we need to specify default_plot_name as the first argument of the script
+    # default_plot_name_parts = sys.argv[1].split('.')[0].split('-')[2:]
+    # default_plot_name = '-'.join(default_plot_name_parts)
+
+    # Get current working directory, plotted figures will be saved at CWD/Assets/
+    cwd = os.getcwd()
+
+    # Parse arguments
+    # Note that argparse.BooleanOptionalAction needs Python 3.9 and newer
     parser = argparse.ArgumentParser()
-    # Need Python 3.9+
+
     # Parse LTTng log files and save to pickle files
     parser.add_argument('--parse', default=False, action=argparse.BooleanOptionalAction)
     # If need parse, we need to provide the log path
     parser.add_argument('-f','--filepath', default=default_lttng_log, type=str, help='Pathname to the LTTng log file')
-    # Read the pickle files and save to json files
+    # Need to read the pickle files and save to json files
     parser.add_argument('--json', default=False, action=argparse.BooleanOptionalAction)
     # Need to plot input and/or output coverage
     parser.add_argument('--plot', default=False, action=argparse.BooleanOptionalAction)
@@ -142,6 +182,8 @@ if __name__ == "__main__":
     parser.add_argument('-i', '--ploti', default=False, action=argparse.BooleanOptionalAction)
     # Plot output coverage only
     parser.add_argument('-o', '--ploto', default=False, action=argparse.BooleanOptionalAction)
+
+    # Parse from the second argument because the first argument is the default_plot_name
     args = parser.parse_args(sys.argv[2:])
     if args.parse and args.filepath is None:
         parser.error("--parse requires --filepath.")
