5e52e80905b65b77c8d6715950c9802a1e725f6c
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-12-19 21:21:02 -0500

    Update copyrights

--- Syzkaller/syscall_parse_new.py
+++ Syzkaller/syscall_parse_new.py
@@ -2,6 +2,7 @@
 
 #
 # Copyright (c) 2020-2024 Yifei Liu
+# Copyright (c) 2020-2024 Rohan Bansal
 # Copyright (c) 2020-2024 Erez Zadok
 # Copyright (c) 2020-2024 Stony Brook University
 # Copyright (c) 2020-2024 The Research Foundation of SUNY
